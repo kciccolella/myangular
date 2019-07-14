44c8af972f79cf0bda428ef33e4898f21c863028
WhoIsThat1 <26233701+WhoIsThat1@users.noreply.github.com>
2019-07-14 09:37:16 -0400

    Chapter 8.2

--- src/parse.js
+++ src/parse.js
@@ -8,7 +8,10 @@ var ESCAPES = {'n':'\n', 'f':'\f', 'r':'\r', 't':'\t',
 var OPERATORS = {
   '+': true,
   '!': true,
-  '-': true
+  '-': true,
+  '*': true,
+  '/': true,
+  '%': true
 };
 
 var CALL = Function.prototype.call;
@@ -194,6 +197,7 @@ AST.MemberExpression = 'MemberExpression';
 AST.CallExpression = 'CallExpression';
 AST.AssignmentExpression = 'AssignmentExpression';
 AST.UnaryExpression = 'UnaryExpression';
+AST.BinaryExpression = 'BinaryExpression';
 
 AST.prototype.ast = function(text) {
   this.tokens = this.lexer.lex(text);
@@ -328,9 +332,9 @@ AST.prototype.parseArguments = function() {
 };
 
 AST.prototype.assignment = function() {
-  var left = this.unary();
+  var left = this.multiplicative();
   if (this.expect('=')) {
-    var right = this.unary();
+    var right = this.multiplicative();
     return {type: AST.AssignmentExpression, left: left, right: right};
   }
   return left;
@@ -347,6 +351,20 @@ AST.prototype.unary = function() {
   } else {
     return this.primary();
   }
+};
+
+AST.prototype.multiplicative = function() {
+  var left = this.unary();
+  var token;
+  while ((token = this.expect('*', '/', '%'))) {
+    left = {
+      type: AST.BinaryExpression,
+      left: left,
+      operator: token.text,
+      right: this.unary()
+    };
+  }
+  return left;
 };
 
 function ASTCompiler(astBuilder) {
@@ -488,6 +506,10 @@ ASTCompiler.prototype.recurse = function(ast, context, create) {
   case AST.UnaryExpression:
     return ast.operator +
       '(' + this.ifDefined(this.recurse(ast.argument), 0) + ')';
+  case AST.BinaryExpression:
+    return '(' + this.recurse(ast.left) + ')' +
+      ast.operator +
+      '(' + this.recurse(ast.right) + ')';
   }
 };
 
