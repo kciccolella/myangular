5f90474b565b04cea7e4f81d3a66bdc51f9fc86b
WhoIsThat1 <26233701+WhoIsThat1@users.noreply.github.com>
2019-09-19 08:52:03 -0400

    Chapter 10.6

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -2,6 +2,7 @@
 
 var _ = require('lodash');
 var Scope = require('../src/scope');
+var register = require('../src/filter').register;
 
 describe('Scope', function() {
 
@@ -494,7 +495,7 @@ describe('Scope', function() {
       expect(scope.$$watchers.length).toBe(0);
     });
 
-    fit('does not re-evaluate an array if its contents do not change', function() {
+    it('does not re-evaluate an array if its contents do not change', function() {
       var values = [];
 
       scope.a = 1;
@@ -516,6 +517,29 @@ describe('Scope', function() {
       scope.$digest();
       expect(values.length).toBe(2);
       expect(values[1]).toEqual([1, 2, 4]);
+    });
+
+    it('allows $stateful filter value to change over time', function(done) {
+
+      register('withTime', function() {
+        return _.extend(function(v) {
+          return new Date().toISOString() + ':' + v;
+        }, {
+          $stateful: true
+        });
+      });
+
+      var listenerSpy = jasmine.createSpy();
+      scope.$watch('42 | withTime', listenerSpy);
+      scope.$digest();
+      var firstValue = listenerSpy.calls.mostRecent().args[0];
+
+      setTimeout(function() {
+        scope.$digest();
+        var secondValue = listenerSpy.calls.mostRecent().args[0];
+        expect(secondValue).not.toEqual(firstValue);
+        done();
+      }, 100);
     });
 
   });
