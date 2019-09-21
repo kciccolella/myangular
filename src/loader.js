76da2d5e75c515ab32142001694581757b527df7
WhoIsThat1 <26233701+WhoIsThat1@users.noreply.github.com>
2019-09-21 09:16:38 -0400

    Chapter 11.3

--- src/loader.js
+++ src/loader.js
@@ -6,6 +6,10 @@ function setupModuleLoader(window) {
 	};
 
 	var angular = ensure(window, 'angular', Object);
+
+	ensure(angular, 'module', function() {
+		return function() {};
+	});
 }
 
 module.exports = setupModuleLoader;
