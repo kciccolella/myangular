3e5aedbdfa2e6b1b588da921741f337b6597113f
WhoIsThat1 <26233701+WhoIsThat1@users.noreply.github.com>
2019-10-29 08:48:51 -0400

    Chapter 11

--- src/injector.js
+++ src/injector.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var _ = require('lodash');
+
 var FN_ARGS = /^function\s*[^\(]*\(\s*([^\)]*)\)/m;
 var FN_ARG = /^\s*(_?)(\S+?)\1\s*$/;
 var STRIP_COMMENTS = /(\/\/.*$)|(\/\*.*?\*\/)/mg;
@@ -55,6 +56,13 @@ function createInjector(modulesToLoad, strictDi) {
     return fn.apply(self, args);
   }
 
+  function instantiate(Type, locals) {
+    var UnwrappedType = _.isArray(Type) ? _.last(Type) : Type;
+    var instance = Object.create(UnwrappedType.prototype);
+    invoke(Type, instance, locals);
+    return instance;
+  }
+
   _.forEach(modulesToLoad, function loadModule(moduleName) {
     if (!loadedModules.hasOwnProperty(moduleName)) {
       loadedModules[moduleName] = true;
@@ -76,7 +84,8 @@ function createInjector(modulesToLoad, strictDi) {
       return cache[key];
     },
     annotate: annotate,
-    invoke: invoke
+    invoke: invoke,
+    instantiate: instantiate
   };
 }
 
