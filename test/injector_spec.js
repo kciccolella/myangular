d9e675a9a44b8608e97247a07b17cf61de45e684
WhoIsThat1 <26233701+WhoIsThat1@users.noreply.github.com>
2019-10-30 09:20:18 -0400

    Chapter 12.1

--- test/injector_spec.js
+++ test/injector_spec.js
@@ -309,6 +309,20 @@ describe('injector', function() {
       expect(instance.result).toBe(4);
     });
 
+    it('allows registering a provider and uses its $get', function() {
+      var module = window.angular.module('myModule', []);
+      module.provider('a', {
+        $get: function() {
+          return 42;
+        }
+      });
+
+      var injector = createInjector(['myModule']);
+
+      expect(injector.has('a')).toBe(true);
+      expect(injector.get('a')).toBe(42);
+    });
+
   });
 
 });
