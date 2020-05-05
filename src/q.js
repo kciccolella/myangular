e733f402a17290bc7cca62e4f84ed056fac70e62
WhoIsThat1 <26233701+WhoIsThat1@users.noreply.github.com>
2020-05-05 09:08:33 -0400

    Chapter 14.6

--- src/q.js
+++ src/q.js
@@ -4,6 +4,21 @@ function $QProvider() {
 
   this.$get = function() {
 
+    function Promise() {
+    }
+
+    function Deferred() {
+      this.promise = new Promise();
+    }
+
+    function defer() {
+      return new Deferred();
+    }
+
+    return {
+      defer: defer
+    };
+
   };
 
 }
