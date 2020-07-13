a132b2463d4111e24ca4fcd1c88b37464c111be6
kciccolella <26233701+kciccolella@users.noreply.github.com>
2020-07-13 09:01:34 -0400

    Chapter 15.3

--- src/http.js
+++ src/http.js
@@ -1,7 +1,36 @@
 'use strict';
 
+function isSuccess(status) {
+  return status >= 200 && status < 300;
+}
+
 function $HttpProvider() {
 
-  this.$get = ['$httpBackend', function($httpBackend) {}];
+  this.$get = ['$httpBackend', '$q', '$rootScope',
+    function($httpBackend, $q, $rootScope) {
+
+    return function $http(config) {
+      var deferred = $q.defer();
+
+      function done(status, response, statusText) {
+        status = Math.max(status, 0);
+        deferred[isSuccess(status) ? 'resolve' : 'reject']({
+          status: status,
+          data: response,
+          statusText: statusText,
+          config: config
+        });
+        if (!$rootScope.$$phase) {
+          $rootScope.$apply();
+        }
+      }
+
+      $httpBackend(config.method, config.url, config.data, done);
+      return deferred.promise;
+    };
+
+  }];
   
 }
+
+module.exports = $HttpProvider;
