943a403c941994244cac0e90c60d95cb2048fca5
kciccolella <26233701+kciccolella@users.noreply.github.com>
2020-07-22 08:50:51 -0400

    Chapter 15.6

--- src/http.js
+++ src/http.js
@@ -25,52 +25,6 @@ function $HttpProvider() {
     }
   };
 
-  this.$get = ['$httpBackend', '$q', '$rootScope',
-    function($httpBackend, $q, $rootScope) {
-
-    function $http(requestConfig) {
-      var deferred = $q.defer();
-
-      var config = _.extend({
-        method: 'GET'
-      }, requestConfig);
-      config.headers = mergeHeaders(requestConfig);
-
-      if(_.isUndefined(config.data)) {
-        _.forEach(config.headers, function(v, k) {
-          if (k.toLowerCase() === 'content-type') {
-            delete config.headers[k];
-          }
-        });
-      }
-
-      function done(status, response, statusText) {
-        status = Math.max(status, 0);
-        deferred[isSuccess(status) ? 'resolve' : 'reject']({
-          status: status,
-          data: response,
-          statusText: statusText,
-          config: config
-        });
-        if (!$rootScope.$$phase) {
-          $rootScope.$apply();
-        }
-      }
-
-      $httpBackend(
-        config.method,
-        config.url,
-        config.data,
-        done,
-        config.headers
-      );
-      return deferred.promise;
-    }
-    $http.defaults = defaults;
-    return $http;
-
-  }];
-
   function executeHeaderFns(headers, config) {
     return _.transform(headers, function(result, v, k) {
       if (_.isFunction(v)) {
@@ -104,7 +58,78 @@ function $HttpProvider() {
     });
     return executeHeaderFns(reqHeaders, config);
   }
-  
+
+  function parseHeaders(headers) {
+    var lines = headers.split('\n');
+    return _.transform(lines, function(result, line) {
+      var separatorAt = line.indexOf(':');
+      var name = _.trim(line.substr(0, separatorAt)).toLowerCase();
+      var value = _.trim(line.substr(separatorAt + 1));
+      if (name) {
+        result[name] = value;
+      }
+    }, {});
+  }
+
+  function headersGetter(headers) {
+    var headersObj;
+    return function(name) {
+      headersObj = headersObj || parseHeaders(headers);
+      if (name) {
+        return headersObj[name.toLowerCase()];
+      } else {
+        return headersObj;
+      }
+    };
+  }
+
+  this.$get = ['$httpBackend', '$q', '$rootScope',
+    function($httpBackend, $q, $rootScope) {
+
+    function $http(requestConfig) {
+      var deferred = $q.defer();
+
+      var config = _.extend({
+        method: 'GET'
+      }, requestConfig);
+      config.headers = mergeHeaders(requestConfig);
+
+      if(_.isUndefined(config.data)) {
+        _.forEach(config.headers, function(v, k) {
+          if (k.toLowerCase() === 'content-type') {
+            delete config.headers[k];
+          }
+        });
+      }
+
+      function done(status, response, headersString, statusText) {
+        status = Math.max(status, 0);
+        deferred[isSuccess(status) ? 'resolve' : 'reject']({
+          status: status,
+          data: response,
+          statusText: statusText,
+          headers: headersGetter(headersString),
+          config: config
+        });
+        if (!$rootScope.$$phase) {
+          $rootScope.$apply();
+        }
+      }
+
+      $httpBackend(
+        config.method,
+        config.url,
+        config.data,
+        done,
+        config.headers
+      );
+      return deferred.promise;
+    }
+    $http.defaults = defaults;
+    return $http;
+
+  }];
+
 }
 
 module.exports = $HttpProvider;
