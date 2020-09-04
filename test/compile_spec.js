024e8182de15dc170df21bf81a033958bf73227d
kciccolella <26233701+kciccolella@users.noreply.github.com>
2020-09-04 07:11:10 -0400

    Chapter 16.5

--- test/compile_spec.js
+++ test/compile_spec.js
@@ -127,4 +127,24 @@ describe('$compile', function() {
 		});
 	});
 
+	_.forEach(['x', 'data'], function(prefix) {
+		_.forEach([':', '-', '_'], function(delim) {
+
+			it ('compiles element directives with '+prefix+delim+' prefix', function() {
+				var injector = makeInjectorWithDirectives('myDir', function() {
+					return {
+						compile: function(element) {
+							element.data('hasCompiled', true);
+						}
+					};
+				});
+				injector.invoke(function($compile) {
+					var el = $('<'+prefix+delim+'my-dir></'+prefix+delim+'my-dir>');
+					$compile(el);
+					expect(el.data('hasCompiled')).toBe(true);
+				});
+			});
+		});
+	});
+
 });
