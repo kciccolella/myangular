138e386b0868a1bc8fd4b94bc5ef306fa0269ecb
Krishnal Ciccolella <ciccolella.krishnal@gmail.com>
2020-12-22 10:36:52 -0500

    Chapter 19.7

--- src/controller.js
+++ src/controller.js
@@ -2,6 +2,15 @@
 
 var _ = require('lodash');
 
+function addToScope(locals, identifier, instance) {
+  if (locals && _.isObject(locals.$scope)) {
+    locals.$scope[identifier] = instance;
+  } else {
+    throw 'Cannot export controller as ' + identifier +
+      '! No $scope object provided via locals';
+  }
+}
+
 function $ControllerProvider() {
 
   var controllers = {};
@@ -21,15 +30,19 @@ function $ControllerProvider() {
 
   this.$get = ['$injector', function($injector) {
 
-    return function(ctrl, locals) {
-      if(_.isString(ctrl)) {
+    return function(ctrl, locals, identifier) {
+      if (_.isString(ctrl)) {
         if (controllers.hasOwnProperty(ctrl)) {
           ctrl = controllers[ctrl];
         } else if (globals) {
           ctrl = window[ctrl];
         }
       }
-      return $injector.instantiate(ctrl, locals);
+      var instance = $injector.instantiate(ctrl, locals);
+      if (identifier) {
+        addToScope(locals, identifier, instance);
+      }
+      return instance;
     };
 
   }];
