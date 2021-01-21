cee6efd528848eca6c81584d6dd2fb983c1d3d34
Krishnal Ciccolella <ciccolella.krishnal@gmail.com>
2021-01-21 09:10:27 -0500

    Chapter 20.2

--- src/compile.js
+++ src/compile.js
@@ -530,14 +530,16 @@ function $CompileProvider($provide) {
               linkFn.pre, linkFn.post, attrStart, attrEnd, isolateScope, require);
           }
         }
-        if (directive.terminal) {
-          terminal = true;
-          terminalPriority = directive.priority;
-        }
-
         if(directive.controller) {
           controllerDirectives = controllerDirectives || {};
           controllerDirectives[directive.name] = directive;
+        }
+        if(directive.template) {
+          $compileNode.html(directive.template);
+        }
+        if (directive.terminal) {
+          terminal = true;
+          terminalPriority = directive.priority;
         }
       });
 
