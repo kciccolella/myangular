28e7d15ec99f2383357a02978a57f5462ae3661e
kciccolella <26233701+kciccolella@users.noreply.github.com>
2021-02-15 09:16:41 -0500

    Chapter 21.3

--- src/compile.js
+++ src/compile.js
@@ -196,9 +196,14 @@ function $CompileProvider($provide) {
     function compile($compileNodes) {
       var compositeLinkFn = compileNodes($compileNodes);
 
-      return function publicLinkFn(scope) {
+      return function publicLinkFn(scope, options) {
+        options = options || {};
+        var parentBoundTranscludeFn = options.parentBoundTranscludeFn;
+        if (parentBoundTranscludeFn && parentBoundTranscludeFn.$$boundTransclude) {
+          parentBoundTranscludeFn = parentBoundTranscludeFn.$$boundTransclude;
+        }
         $compileNodes.data('$scope', scope);
-        compositeLinkFn(scope, $compileNodes);
+        compositeLinkFn(scope, $compileNodes, parentBoundTranscludeFn);
         return $compileNodes;
       };
     }
@@ -229,7 +234,7 @@ function $CompileProvider($provide) {
         }
       });
 
-      function compositeLinkFn(scope, linkNodes) {
+      function compositeLinkFn(scope, linkNodes, parentBoundTranscludeFn) {
         var stableNodeList = [];
         _.forEach(linkFns, function(linkFn) {
           var nodeIdx = linkFn.idx;
@@ -255,6 +260,8 @@ function $CompileProvider($provide) {
                 }
                 return linkFn.nodeLinkFn.transclude(transcludedScope);
               };
+            } else if (parentBoundTranscludeFn) {
+              boundTranscludeFn = parentBoundTranscludeFn;
             }
 
             linkFn.nodeLinkFn(
@@ -266,7 +273,8 @@ function $CompileProvider($provide) {
           } else {
             linkFn.childLinkFn(
               scope,
-              node.childNodes
+              node.childNodes,
+              parentBoundTranscludeFn
             );
           }
         });
@@ -700,6 +708,7 @@ function $CompileProvider($provide) {
         function scopeBoundTranscludeFn(transcludedScope) {
           return boundTranscludeFn(transcludedScope, scope);
         }
+        scopeBoundTranscludeFn.$$boundTransclude = boundTranscludeFn;
 
         _.forEach(preLinkFns, function(linkFn) {
           linkFn(
@@ -717,7 +726,7 @@ function $CompileProvider($provide) {
                newIsolateScopeDirective.templateUrl === null)) {
             scopeToChild = isolateScope;
           }
-          childLinkFn(scopeToChild, linkNode.childNodes);
+          childLinkFn(scopeToChild, linkNode.childNodes, boundTranscludeFn);
         }
         _.forEachRight(postLinkFns, function(linkFn) {
           linkFn(
