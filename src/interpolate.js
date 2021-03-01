4ff8d55e2b4ecd7c2f277a4449e9c643d6c12636
kciccolella <26233701+kciccolella@users.noreply.github.com>
2021-03-01 08:55:16 -0500

    Chapter 22.1

---
+++ src/interpolate.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function $InterpolateProvider() {
+
+  this.$get = function() {
+
+    function $interpolate() {}
+
+    return $interpolate;
+  };
+
+}
+
+module.exports = $InterpolateProvider;
