0540605616c5041ee5219521431c05c80fc032ee
kciccolella <26233701+kciccolella@users.noreply.github.com>
2021-03-04 09:02:23 -0500

    Chapter 22.4

--- src/interpolate.js
+++ src/interpolate.js
@@ -16,6 +16,11 @@ function $InterpolateProvider() {
 
   this.$get = ['$parse', function($parse) {
 
+    function unescapeText(text) {
+      return text.replace(/\\{\\{/g, '{{')
+                 .replace(/\\}\\}/g, '}}');
+    }
+
     function $interpolate(text) {
       var index = 0;
       var parts = [];
@@ -27,14 +32,14 @@ function $InterpolateProvider() {
         }
         if (startIndex !== -1 && endIndex !== -1) {
           if (startIndex !== index) {
-            parts.push(text.substring(index, startIndex));
+            parts.push(unescapeText(text.substring(index, startIndex)));
           }
           exp = text.substring(startIndex + 2, endIndex);
           expFn = $parse(exp);
           parts.push(expFn);
           index = endIndex + 2;
         } else {
-          parts.push(text.substring(index));
+          parts.push(unescapeText(text.substring(index)));
           break;
         }
       }
